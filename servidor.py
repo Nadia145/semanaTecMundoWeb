1f90f4d626120c6a38fed34ffc6af4c50bc77e6b
Nadia <nadiapaola145@gmail.com>
2022-10-25 09:59:47 -0500

    Actualizacion

--- servidor.py
+++ servidor.py
@@ -1,5 +1,8 @@
-from distutils.log import debug
 from flask import Flask, request, jsonify, render_template
+from werkzeug.utils import secure_filename
+from joblib import load
+import numpy as np
+import os
 
 
 # Generar el servidor (Back-end)
@@ -12,6 +15,31 @@ servidorWeb = Flask(__name__)
 @servidorWeb.route("/test", methods=['GET'])
 def formulario():
     return render_template('pagina.html')
+
+
+# Procesar datos a traves del form
+@servidorWeb.route('/modeloIA', methods=["POST"])
+def modeloForm():
+    # Procesar los datos de entrada
+
+    contendio = request.form
+
+    print(contendio)
+
+    return jsonify({"Resultado": "datos recibidos"})
+
+
+# Procesar datos de un archivo
+@servidorWeb.route('/modeloFile', methods=['POST'])
+def modeloFile():
+    f = request.files['file']
+    filename = secure_filename(f.filename)
+    path = os.path.join(os.getcwd, 'files', filename)
+    f.save(path)
+    file = open(path, 'r')
+    for line in file:
+        print(line)
+    return jsonify({"Resultado": "datos recibidos"})
 
 
 if __name__ == '__main__':
